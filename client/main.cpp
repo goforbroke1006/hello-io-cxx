734d5bb9a1eb79d817066f5510b96c7ec1f511c2
goforbroke <go.for.broke1006@gmail.com>
2021-05-16 19:58:01 +0300

    fixes

--- client/main.cpp
+++ client/main.cpp
@@ -27,8 +27,8 @@ int main(int argc, char **argv) {
 
     struct sockaddr_in serv_addr;
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
+    int clientFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientFD < 0) {
         perror("socket creation");
         exit(EXIT_FAILURE);
     }
@@ -38,13 +38,14 @@ int main(int argc, char **argv) {
 
     // Convert IPv4 and IPv6 addresses from text to binary form
     if (inet_pton(AF_INET, serverHost, &serv_addr.sin_addr) <= 0) {
-        perror("inet_pton");
-        return -1;
+        perror("  inet_pton() failed");
+        exit(EXIT_FAILURE);
     }
 
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        perror("connect");
-        return -1;
+    if (connect(clientFD, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("  connect() failed");
+        close(clientFD);
+        exit(EXIT_FAILURE);
     }
 
     std::vector<std::string> messages = {
@@ -65,27 +66,32 @@ int main(int argc, char **argv) {
     auto startSpan = std::chrono::high_resolution_clock::now();
     std::string response;
 
+    bool hasErrors = false;
+
     for (const auto &msg : messages) {
-        ssize_t sendLen = send(sockfd, msg.c_str(), msg.length(), 0);
+        ssize_t sendLen = send(clientFD, msg.c_str(), msg.length(), 0);
         if (sendLen < 0) {
             perror("  send() failed");
-            exit(EXIT_FAILURE);
+            hasErrors = true;
+            break;
         }
         if (((size_t) sendLen) != msg.length()) {
             std::cerr << "  Unexpected sent message length: got " << sendLen << " want " << msg.length() << std::endl;
-            exit(EXIT_FAILURE);
+            hasErrors = true;
+            break;
         }
 
         memset(buffer, 0, sizeof(buffer));
-        ssize_t recvLen = recv(sockfd, buffer, 1024, 0);
+        ssize_t recvLen = recv(clientFD, buffer, 1024, 0);
 
         if (recvLen < 0) {
             perror("  recv() failed");
-            exit(EXIT_FAILURE);
+            hasErrors = true;
+            break;
         }
         if (recvLen == 0) {
             // communication finished
-            close(sockfd);
+            close(clientFD);
             break;
         }
 
@@ -94,15 +100,23 @@ int main(int argc, char **argv) {
 
         if ("pong" != response) {
             std::cerr << "Unexpected response: \"" << response << "\"" << std::endl;
-            exit(EXIT_FAILURE);
+            hasErrors = true;
+            break;
         }
-//        std::cout << ">>> (" << valread << "): " << buffer << std::endl;
     }
 
+    // finalize
+    close(clientFD);
+
+    // on errors
+    if (hasErrors) {
+        exit(EXIT_FAILURE);
+    }
+
+    // summary
     auto endSpan = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endSpan - startSpan).count();
     std::cout << "SPEND TIME: " << duration << " milliseconds" << std::endl;
 
-    close(sockfd);
-    return 0;
+    exit(EXIT_SUCCESS);
 }
